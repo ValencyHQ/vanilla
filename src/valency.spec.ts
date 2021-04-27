e10e6138c70d96bac4b95e24f1070fe5aae1396f
ahkohd <victor.olorunbunmi@gmail.com>
2021-04-27 15:54:25 +0100

    feat: Initial commit

---
+++ src/valency.spec.ts
@@ -0,0 +1,66 @@
+import { Valency } from './valency'
+
+const BASE_URL = 'https://cdn.valency.design/'
+const defaultConfig = {
+      username: 'ahkohd',
+      defaultProjectId: 'id',
+      defaultLibraryId: '123',
+}
+
+describe('Test Valency class', () => {
+      afterEach(() => {
+            document.body.innerHTML = ''
+      })
+
+      describe('Check if base URL is correct', () => {
+            it(`shoud return: ${BASE_URL}`, () => {
+                  expect(Valency.baseUrl).toEqual(BASE_URL)
+            })
+      })
+
+      describe('Test get() method', () => {
+            it(`should return`, () => {
+                  const assetName = 'cat-dog'
+                  const assetURL = new Valency(defaultConfig).get(assetName)
+
+                  expect(assetURL).toEqual(
+                        `${BASE_URL}${defaultConfig.username}/${defaultConfig.defaultProjectId}/${defaultConfig.defaultLibraryId}/${assetName}`
+                  )
+            })
+      })
+
+      describe('Test replace() method', () => {
+            it(`should set src attribute to asset's URL of IMG element with data-valency="cat-dog"`, () => {
+                  document.body.innerHTML = `<img data-valency="cat-dog">`.trim()
+
+                  new Valency(defaultConfig).replace(undefined, document)
+
+                  expect(document.body.innerHTML).toBe(
+                        `<img data-valency="cat-dog" src="${`${BASE_URL}${defaultConfig.username}/${defaultConfig.defaultProjectId}/${defaultConfig.defaultLibraryId}/cat-dog`}">`
+                  )
+            })
+
+            it(`should set data attribute to asset's URL of OBJECT element with data-valency="cat-dog"`, () => {
+                  document.body.innerHTML = `<object data-valency="cat-dog"></object>`.trim()
+
+                  new Valency(defaultConfig).replace(undefined, document)
+
+                  expect(document.body.innerHTML).toBe(
+                        `<object data-valency="cat-dog" data="${`${BASE_URL}${defaultConfig.username}/${defaultConfig.defaultProjectId}/${defaultConfig.defaultLibraryId}/cat-dog`}"></object>`
+                  )
+            })
+
+            it(`should set background image to asset's URL of DIV element with data-valency="cat-dog"`, () => {
+                  document.body.innerHTML = `<div data-valency="cat-dog"></div>`
+
+                  new Valency(defaultConfig).replace(undefined, document)
+
+                  expect(
+                        (document.body.children[0] as HTMLDivElement).style
+                              .backgroundImage
+                  ).toBe(
+                        `url(${BASE_URL}${defaultConfig.username}/${defaultConfig.defaultProjectId}/${defaultConfig.defaultLibraryId}/cat-dog)`
+                  )
+            })
+      })
+})
---
+++ src/valency.ts
@@ -0,0 +1,81 @@
+export type ValencyConfig = {
+      username: string
+      defaultLibraryId: string
+      defaultProjectId: string
+}
+
+export type Config = {
+      username?: string
+      project?: string
+      library?: string
+}
+
+export class Valency {
+      public static baseUrl = 'https://cdn.valency.design/'
+
+      constructor(private baseConfig: ValencyConfig) {}
+
+      /**
+       * Returns configuration
+       */
+
+      getConfig(otherConfig?: Config): Config {
+            return {
+                  username: otherConfig?.username ?? this.baseConfig.username,
+                  project:
+                        otherConfig?.project ??
+                        this.baseConfig.defaultProjectId,
+                  library:
+                        otherConfig?.library ??
+                        this.baseConfig.defaultLibraryId,
+            }
+      }
+
+      /**
+       *
+       * @param assetName
+       * @param config (optional) If not provided, will use the base config
+       * @returns a URL to the provided `assetName`
+       */
+
+      get(assetName: string, config?: Config): string {
+            const preset = this.getConfig(config)
+
+            return `${Valency.baseUrl}${preset.username}/${preset.project}/${preset.library}/${assetName}`
+      }
+
+      /**
+       * All elements that have a data-valency attribute, their
+       * src atrribute will be replaced with the assset URL corresponding to their
+       * data-valency attribute value.
+       * See the API Reference for more information about feather.replace().
+       *
+       * @param config (optional) If not provided, will use the base config
+       */
+
+      replace(config?: Config, document?: HTMLDocument): void {
+            const doc = document ?? window.document
+
+            ;[...doc.querySelectorAll('[data-valency]')].forEach((node) => {
+                  const element = node as HTMLElement
+                  const assetName = element.dataset.valency
+
+                  if (element.tagName === 'IMG') {
+                        ;(element as HTMLImageElement).src = this.get(
+                              assetName ?? '',
+                              config
+                        )
+                  } else if (element.tagName === 'DIV') {
+                        element.style.backgroundImage = `url(${this.get(
+                              assetName ?? '',
+                              config
+                        )})`
+                  } else if (element.tagName === 'OBJECT') {
+                        ;(element as HTMLObjectElement).data = this.get(
+                              assetName ?? '',
+                              config
+                        )
+                  }
+            })
+      }
+}
