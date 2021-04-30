357912d9fa5c975a04e1096229b59856116e8964
ahkohd <victor.olorunbunmi@gmail.com>
2021-04-30 15:34:42 +0100

    fix: make eslint ignore src folder

--- scripts/get-project-name.js
+++ scripts/get-project-name.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const easyjson = require('easyjson')
 
-// eslint-disable-next-line no-console
 console.log(easyjson.path('package').get('name'));
--- scripts/get-project-version.js
+++ scripts/get-project-version.js
@@ -1,6 +1,4 @@
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const easyjson = require('easyjson')
 
-// eslint-disable-next-line no-console
 console.log(easyjson.path('package').get('version'));
