9f7c560ec7f25c1838f390a88a2fad1e4e5703cd
ahkohd <victor.olorunbunmi@gmail.com>
2021-04-30 12:35:35 +0100

    Initial relase

--- scripts/get-project-name.js
+++ scripts/get-project-name.js
@@ -1,5 +1,5 @@
-
-import { path } from 'easyjson';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { path } = require('easyjson')
 
 // eslint-disable-next-line no-console
 console.log(path('package').get('name'));
--- scripts/get-project-version.js
+++ scripts/get-project-version.js
@@ -1,5 +1,6 @@
 
-import { path } from 'easyjson';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { path } = require('easyjson')
 
 // eslint-disable-next-line no-console
 console.log(path('package').get('version'));
